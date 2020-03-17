309babd9f8942394be17e9ddd4f37f1c8ffa04fd
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-03-17 16:56:17 +0900

    message_model create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update] 
   resources :groups, only: [:new, :create, :edit, :update, :index]
+  resources :groups do
+    resources :users do
+      resources :messages, only: [:create]
+    end 
+  end 
 end
